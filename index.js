b60ed0f5ea433a3267532ce0cfd5955453baa773
VinhOP <lyquocvinh03@gmail.com>
2023-07-19 19:52:59 +0700

    change node engine

--- index.js
+++ index.js
@@ -24,4 +24,13 @@ app.use(`/products`, productRouter);
 app.use(`/category`, categoryRouter);
 app.use(`/admin`, adminRouter);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(PORT);
